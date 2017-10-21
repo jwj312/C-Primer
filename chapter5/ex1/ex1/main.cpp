f025ad1fcca99b0f97d3c74c8a0a3f456911b2fe
정원준 <jwj312@naver.com>
2017-10-21 13:43:00 +0900

    chapter 5 - 1 연습완료

---
+++ chapter5/ex1/ex1/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ex1
+//
+//  Created by 정원준 on 2017. 10. 21..
+//  Copyright © 2017년 wonjun. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    
+    for ( i=0; i<5; ++i)
+        cout << "C++는 루프를 압니다.\n";
+    cout << "C++는 멈출 시기를 압니다.\n";
+    return 0;
+}
