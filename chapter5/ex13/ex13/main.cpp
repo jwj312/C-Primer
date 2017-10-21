8fd8aaab340996d0bce1ce519afdbb351352e069
정원준 <jwj312@naver.com>
2017-10-21 17:52:11 +0900

    chapter 5 - 13 연습완료

---
+++ chapter5/ex13/ex13/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  ex13
+//
+//  Created by 정원준 on 2017. 10. 21..
+//  Copyright © 2017년 wonjun. All rights reserved.
+//
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+    cout << "지연 시간을 초 단위로 입력하고 Enter 키를 누르십시오: ";
+    float secs; cin >> secs;
+    clock_t delay = secs * CLOCKS_PER_SEC;
+    
+    cout << "카운트를 시작합니다.\a\n";
+    clock_t start = clock();
+    while (clock() - start < delay )
+        ;
+    cout << "마침\a\n";
+    return 0;
+}
