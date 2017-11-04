7e49a529e79a2207e9450a7a1f8996a8dd4b5130
정원준 <jwj312@naver.com>
2017-11-04 16:21:39 +0900

    chapter 7 - 1 연습완료

---
+++ chapter7/ex1/ex1/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  ex1
+//
+//  Created by 정원준 on 2017. 11. 4..
+//  Copyright © 2017년 wonjun. All rights reserved.
+//
+
+#include <iostream>
+#include "simple.h"
+using namespace std;
+
+int main()
+{
+    cout << "main()에서 simple() 함수를 호출하게 됩니다.\n";
+    simple();
+    return 0;
+}
+
+
---
+++ chapter7/ex1/ex1/simple.h
@@ -0,0 +1,17 @@
+//
+//  simple.h
+//  ex1
+//
+//  Created by 정원준 on 2017. 11. 4..
+//  Copyright © 2017년 wonjun. All rights reserved.
+//
+
+#ifndef simple_h
+#define simple_h
+void simple()
+{
+    std::cout << "나는 간단한(simple) 함수입니다.\n";
+}
+
+
+#endif /* simple_h */
